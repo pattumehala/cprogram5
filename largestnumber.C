091988a3a2c77f8be7ca5b35197ef269fde714b2
pattumehala <35220817+pattumehala@users.noreply.github.com>
2018-01-18 21:14:55 +0530

    largestnumber.C
    greatest number among three numbers

--- largestnumber.C
+++ largestnumber.C
@@ -4,15 +4,15 @@ int main()
 int x,y,z;
 printf("Enter the three numbers");
 scanf("%d %d %d",&x,&y,&z);
-if((x>y)&&(x>z))
+if(x>y && x>z)
 {
 printf("the largest number is:",x);
 }
-if((y>x)&&(y>z))
+if(y>x && y>z)
 {
 printf("the largest number is:",y);
 }
-if((z>x)&&(z>y))
+if(z>x && z>y)
 {
 printf("the largest number is:",z);
 }
