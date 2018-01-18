bdb38084b0ee9e45a3d28f2475238fc232e2d686
pattumehala <35220817+pattumehala@users.noreply.github.com>
2018-01-18 19:59:55 +0530

    largestnumber.C
    find the largest number among three numbers

---
+++ largestnumber.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int x,y,z;
+printf("Enter the three numbers");
+scanf("%d %d %d",&x,&y,&z);
+if((x>y)&&(x>z))
+{
+printf("the largest number is:",x);
+}
+if((y>x)&&(y>z))
+{
+printf("the largest number is:",y);
+}
+if((z>x)&&(z>y))
+{
+printf("the largest number is:",z);
+}
+return 0;
+}
+
